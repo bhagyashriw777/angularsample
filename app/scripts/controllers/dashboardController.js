f6ca290e0b1bc80e3dff52861070f0ac487b2b9d
bhagyashriw777 <bhagyashriw777@gmail.com>
2018-04-05 19:06:41 +0530

    updated dashboard controller

--- app/scripts/controllers/dashboardController.js
+++ app/scripts/controllers/dashboardController.js
@@ -1,7 +1,7 @@
 app.controller('dashboardController', function($scope,$http){ 
     $scope.pools = [];
 
-    $http.get('/api/dasboard').
+    $http.get('http://api-testapp.9i2ixvpuxp.us-east-1.elasticbeanstalk.com/api/dashboard').
     success(function(data, status, headers, config) {
         $scope.pools = data;	 
     });/*
