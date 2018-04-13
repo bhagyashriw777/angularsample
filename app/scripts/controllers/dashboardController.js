49e80e0a58f235d6b31fc4060a267a1103d3ff18
bhagyashriw777 <bhagyashriw777@gmail.com>
2018-04-13 17:49:52 +0530

    commit to push data on aws

--- app/scripts/controllers/dashboardController.js
+++ app/scripts/controllers/dashboardController.js
@@ -18,10 +18,16 @@ var app=angular.module('testApp', [
   ]);
     app.controller('dashboardController', function($scope,$http){ 
     $scope.pools = [];
-
-    $http.get('http://api-testapp.9i2ixvpuxp.us-east-1.elasticbeanstalk.com/api/dashboard').success(function(data, status, headers, config) {
-        $scope.pools = data;	 
-    });/*
+    
+    $http.get('http://localhost:3000/api/v1/users', 
+    {headers: { Authorization: ' Token api_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxx'}})
+    .then(function(response) {
+            service.currentUser = response.data.user;
+            console.log(service.currentUser);
+    });
+    //$http.get('http://api-testapp.9i2ixvpuxp.us-east-1.elasticbeanstalk.com/api/dashboard').success(function(data, status, headers, config) {
+      //  $scope.pools = data;	 
+    //});/*
     $scope.addVote = function(pooloptions){
     $http.get('/api/pooloption/addvote/'+ pooloptions.id).
         success(function(data, status, headers, config) {
